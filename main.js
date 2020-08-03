740da7cfc404aab64f6d67585dabf9c104d68bda
eric <eric@sonner.com.br>
2020-08-03 20:49:14 -0300

    Simple class

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-class Teste {
-    metodo() {}
+class List {
+    constructor() {
+        this.data = [];
+    }
 
-    outro() {}
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
 }
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Eric';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+class Matematica {
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+console.log(Matematica.soma(1, 2));
