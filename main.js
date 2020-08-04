215093b0c2d23291d3b95997f1d4ceffc73d50b8
eric <eric@sonner.com.br>
2020-08-03 21:28:41 -0300

    Rest/Spread

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const usuario = {
+// REST
+
+const usuario1 = {
     nome: 'Eric',
     idade: 27,
-    endereco: {
-        cidade: 'Tatui',
-        estado: 'SP',
-    },
+    empresa: 'Sonner'
 };
 
-function mostraNome({ nome, idade, endereco: { cidade } }) {
-    console.log(nome, idade, cidade);
+const { nome, ...resto } = usuario1;
+
+console.log(nome);
+console.log(resto);
+
+const arr = [1, 2, 3, 4];
+
+const [ a, b, ...c ] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function soma(a, b, ...params) {
+    return params.reduce((total, next) => total + next);
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 5, 6, 7));
+
+// SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [ ...arr1, ...arr2 ];
+
+console.log(arr3);
+
+const usuario2 = { ...usuario1, nome: 'Lucas' };
+
+console.log(usuario2);
